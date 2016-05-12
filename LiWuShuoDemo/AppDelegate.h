df67ac31a251573546011b56f7bc3f1fcf915d75
王勋 <ioriginal@163.com>
2016-05-12 20:02:44 +0800

    Initial Commit

---
+++ LiWuShuoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiWuShuoDemo
+//
+//  Created by XUN WANG on 16/5/12.
+//  Copyright © 2016年 XUN WANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiWuShuoDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LiWuShuoDemo
+//
+//  Created by XUN WANG on 16/5/12.
+//  Copyright © 2016年 XUN WANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
